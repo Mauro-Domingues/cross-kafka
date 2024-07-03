53946c8277fb2c6844198774cf6c9c2c872f0300
Mauro-Domingues <maurinho.villa@hotmail.com>
2024-07-03 13:24:01 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-declare module 'cross-kafka' {
-  export { KafkaCore } from '@core/KafkaCore';
-  export { IKafkaConfigDTO as KafkaConfig } from '@interfaces/IKafkaConfigDTO';
-  export { IWritePacketDTO as BaseMessage } from '@interfaces/IProxyDTO';
-}
+export { KafkaCore } from '@core/KafkaCore';
+export { IKafkaConfigDTO as KafkaConfig } from '@interfaces/IKafkaConfigDTO';
+export { IWritePacketDTO as BaseMessage } from '@interfaces/IProxyDTO';
+export { Proxy } from '@base/Proxy';
